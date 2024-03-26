16477a3234a15ee5019a21354ba661213524aa36
Alex Stewart <alex.stewart@dentalit.ltd.uk>
2024-03-26 09:07:06 +0000

    Added default maui project with minimal code to reproduce the bug. Verified still an issue on iOS

---
+++ MauiCollectionViewBug/MauiCollectionViewBug/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiCollectionViewBug;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<string> Collection { get; } = ["This is some text", "This is more text"];
+
+    public MainPage()
+    {
+        BindingContext = this;
+        InitializeComponent();
+    }
+
+}
